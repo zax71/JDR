d5181f000a2922f2b87f31fea507d450c7017da7
zabax32@gmail.com <zkennedy-smith@students.spexe.org>
2021-12-26 17:54:51 +0000

    Make /gmc actually do something!

--- src/main/java/me/zax71/JDR/commands/GMCCommand.java
+++ src/main/java/me/zax71/JDR/commands/GMCCommand.java
@@ -1,11 +1,25 @@
 package me.zax71.JDR.commands;
 
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.color.Color;
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.entity.GameMode;
+import net.minestom.server.entity.Player;
 
 public class GMCCommand extends Command {
 
     public GMCCommand() {
         super("gmc", "gamemodeC", "gamemodeCreative");
 
+        setDefaultExecutor((sender, context) -> {
+            sender.sendMessage(Component.text("Changed to Creative mode").color(NamedTextColor.GREEN));
+
+            // Cast sender to a player object
+            Player senderPlayer = (Player) sender;
+
+            // Set their gamemode
+            senderPlayer.setGameMode(GameMode.CREATIVE);
+        });
     }
 }
