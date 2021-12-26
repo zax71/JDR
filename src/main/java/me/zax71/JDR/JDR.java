c2409ed3034832cf752df5dcfd1bd14d5136dbc7
zabax32@gmail.com <zkennedy-smith@students.spexe.org>
2021-12-26 15:23:06 +0000

    first commit

---
+++ src/main/java/me/zax71/JDR/JDR.java
@@ -0,0 +1,38 @@
+package me.zax71.JDR;
+
+import me.zax71.JDR.ChunkGen.stoneWorld;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.entity.Player;
+import net.minestom.server.event.GlobalEventHandler;
+import net.minestom.server.event.player.PlayerLoginEvent;
+import net.minestom.server.instance.*;
+import net.minestom.server.coordinate.Pos;
+
+public class JDR {
+
+    public static void main(String[] args) {
+
+        // Initialization
+        MinecraftServer minecraftServer = MinecraftServer.init();
+
+        InstanceManager instanceManager = MinecraftServer.getInstanceManager();
+        // Create the instance
+        InstanceContainer instanceContainer = instanceManager.createInstanceContainer();
+        // Set the ChunkGenerator
+        instanceContainer.setChunkGenerator(new stoneWorld());
+
+        // Add an event callback to specify the spawning instance (and the spawn position)
+        GlobalEventHandler globalEventHandler = MinecraftServer.getGlobalEventHandler();
+        globalEventHandler.addListener(PlayerLoginEvent.class, event -> {
+            final Player player = event.getPlayer();
+            event.setSpawningInstance(instanceContainer);
+            player.setRespawnPoint(new Pos(0, 42, 0));
+        });
+
+        // Start the server on port 25565
+        minecraftServer.start("0.0.0.0", 25565);
+    }
+
+
+
+}
